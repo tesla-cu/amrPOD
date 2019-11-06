eaffd319da531042ec4520941770601772ca6f8c
Mike Meehan <michael.a.meehan@colorado.edu>
2019-11-06 15:28:52 -0700

    delete commented code

--- plotting/plot_single.py
+++ plotting/plot_single.py
@@ -8,8 +8,8 @@ def gen_fig(num, den, imgdir, figfile, xaxis, yaxis, xlabel, ylabel):
 	# num - numerator of images
 	# den - denomenator for normalization
 
-	img = np.transpose(np.divide(num, den))
-	# img = np.divide(num, den)
+	# img = np.transpose(np.divide(num, den))
+	img = np.divide(num, den)
 	Xaxis, Yaxis = np.meshgrid(xaxis, yaxis, copy=False, indexing='ij')
 	cont_levs = 100
 	xmin = np.min(xaxis)
@@ -21,10 +21,10 @@ def gen_fig(num, den, imgdir, figfile, xaxis, yaxis, xlabel, ylabel):
 	fig = plt.figure(clear=True)
 	ax = fig.add_subplot(1,1,1)
 
-	# im = ax.contourf(Xaxis, Yaxis, img, cont_levs, origin='lower', \
-	# 	extent=[xmin,xmax,ymin,ymax], cmap='bwr')
-	im = ax.imshow(img, aspect='auto', origin='lower', \
+	im = ax.contourf(Xaxis, Yaxis, img, cont_levs, origin='lower', \
 		extent=[xmin,xmax,ymin,ymax], cmap='bwr')
+	# im = ax.imshow(img, aspect='auto', origin='lower', \
+	# 	extent=[xmin,xmax,ymin,ymax], cmap='bwr')
 	cbar = ax.figure.colorbar(im, ax=ax)
 
 	ax.set_xticks(xaxis)
@@ -98,7 +98,7 @@ def gen_fig(num, den, imgdir, figfile, xaxis, yaxis, xlabel, ylabel):
 
 def plot_single(txtdir, imgdir, compute_tc, compute_cpu, xaxis, yaxis, xlabel, ylabel):
 
-	# ========== Load data
+	# ========== Load data 
 	if compute_tc:
 		TC_R_avg_imp   = np.loadtxt(txtdir + "/TC_R_avg_imp.txt")
 		TC_R_avg_unalt = np.loadtxt(txtdir + "/TC_R_avg_unalt.txt")
@@ -190,172 +190,6 @@ def plot_single(txtdir, imgdir, compute_tc, compute_cpu, xaxis, yaxis, xlabel, y
 			xaxis, yaxis, xlabel, ylabel)
 		gen_fig(CPU_A_rms_imp, CPU_A_avg_imp,   imgdir, 'CPU_A_rms_avg', \
 			xaxis, yaxis, xlabel, ylabel)
-
-
-	"""
-
-	TC_R_imp_avg       = np.loadtxt(txtdir + "/TC_R_imp_avg.txt")
-	TC_R_unalt_avg     = np.loadtxt(txtdir + "/TC_R_unalt_avg.txt")
-	TC_R_imp_rms       = np.loadtxt(txtdir + "/TC_R_imp_rms.txt")
-	TC_R_unalt_rms     = np.loadtxt(txtdir + "/TC_R_unalt_rms.txt")
-
-	TC_Phi_1_imp_avg   = np.loadtxt(txtdir + "/TC_Phi_1_imp_avg.txt")
-	TC_Phi_1_unalt_avg = np.loadtxt(txtdir + "/TC_Phi_1_unalt_avg.txt")
-	TC_Phi_1_imp_rms   = np.loadtxt(txtdir + "/TC_Phi_1_imp_rms.txt")
-	TC_Phi_1_unalt_rms = np.loadtxt(txtdir + "/TC_Phi_1_unalt_rms.txt")
-
-	TC_Phi_5_imp_avg   = np.loadtxt(txtdir + "/TC_Phi_5_imp_avg.txt")
-	TC_Phi_5_unalt_avg = np.loadtxt(txtdir + "/TC_Phi_5_unalt_avg.txt")        
-	TC_Phi_5_imp_rms   = np.loadtxt(txtdir + "/TC_Phi_5_imp_rms.txt")
-	TC_Phi_5_unalt_rms = np.loadtxt(txtdir + "/TC_Phi_5_unalt_rms.txt")
-
-	TC_A_imp_avg       = np.loadtxt(txtdir + "/TC_A_imp_avg.txt")
-	TC_A_unalt_avg	   = np.loadtxt(txtdir + "/TC_A_unalt_avg.txt")
-	TC_A_imp_rms       = np.loadtxt(txtdir + "/TC_A_imp_rms.txt")
-	TC_A_unalt_rms     = np.loadtxt(txtdir + "/TC_A_unalt_rms.txt")
-
-	rc_arr             = np.loadtxt(txtdir + "/rc_array.txt")
-	nt_arr             = np.loadtxt(txtdir + "/nt_array.txt")
-
-	cont_levs          = 100
-
-	# ----------------- Plot R
-	fig_R    	       = plt.figure()
-	TC_R_Norm          = np.divide(TC_R_imp_avg, TC_R_unalt_avg)
-	plt.contourf(nt_arr, rc_arr, TC_R_Norm, cont_levs)
-	cbar 			   = plt.colorbar()
-	cbar.set_label('${T_{imp}}/{T_{unalt}}$')
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('R Efficiency')
-	plt.set_cmap('bwr')
-
-	fig_R_imp_rms      = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_R_imp_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('R Implemented RMS')
-	plt.set_cmap('bwr')
-
-	fig_R_unalt_rms     = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_R_unalt_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('R Unaltered RMS')
-	plt.set_cmap('bwr')
-
-	# ----------------- Plot Phi 1
-	fig_Phi_1	           = plt.figure()
-	TC_Phi_1_Norm          = np.divide(TC_Phi_1_imp_avg, TC_Phi_1_unalt_avg)
-	plt.contourf(nt_arr, rc_arr, TC_Phi_1_Norm, cont_levs)
-	cbar 			       = plt.colorbar()
-	cbar.set_label('${T_{imp}}/{T_{unalt}}$')
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('$Phi_{1}$ Efficiency')
-	plt.set_cmap('bwr')
-
-	fig_Phi_1_imp_rms      = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_Phi_1_imp_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('$Phi_{1}$ Implemented RMS')
-	plt.set_cmap('bwr')
-
-	fig_Phi_1_unalt_rms     = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_Phi_1_unalt_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(range(len(nt_arr)), nt_arr)
-	plt.yticks(range(len(rc_arr)), rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('$Phi_{1}$ Unaltered RMS')
-	plt.set_cmap('bwr')
-
-	# ----------------- Plot Phi 5
-	fig_Phi_5	           = plt.figure()
-	TC_Phi_5_Norm          = np.divide(TC_Phi_5_imp_avg, TC_Phi_5_unalt_avg)
-	plt.contourf(nt_arr, rc_arr, TC_Phi_5_Norm, cont_levs)
-	cbar 			       = plt.colorbar()
-	cbar.set_label('${T_{imp}}/{T_{unalt}}$')
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('$Phi_{5}$ Efficiency')
-	plt.set_cmap('bwr')
-
-	fig_Phi_5_imp_rms      = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_Phi_5_imp_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('$Phi_{5}$ Implemented RMS')
-	plt.set_cmap('bwr')
-
-	fig_Phi_5_unalt_rms     = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_Phi_5_unalt_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('$Phi_{1}$ Unaltered RMS')
-	plt.set_cmap('bwr')
-
-	# ----------------- Plot A
-	fig_A	           = plt.figure()
-	TC_A_Norm          = np.divide(TC_A_imp_avg, TC_A_unalt_avg)
-	plt.contourf(nt_arr, rc_arr, TC_A_Norm, cont_levs)
-	cbar 			   = plt.colorbar()
-	cbar.set_label('${T_{imp}}/{T_{unalt}}$')
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('A Efficiency')
-	plt.set_cmap('bwr')
-
-	fig_A_imp_rms      = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_A_imp_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('A Implemented RMS')
-	plt.set_cmap('bwr')
-
-	fig_A_unalt_rms     = plt.figure()
-	plt.contourf(nt_arr, rc_arr, TC_A_unalt_rms, cont_levs)
-	plt.colorbar()
-	plt.xticks(nt_arr)
-	plt.yticks(rc_arr)
-	plt.xlabel('$n_{t}$')
-	plt.ylabel('$r_{c}$')
-	plt.title('A Unaltered RMS')
-	plt.set_cmap('bwr')
-
-	plt.close('all')
-	
-	return fig_R, fig_R_imp_rms, fig_R_unalt_rms, fig_Phi_1, fig_Phi_1_imp_rms, fig_Phi_1_unalt_rms, fig_Phi_5, fig_Phi_5_imp_rms, fig_Phi_5_unalt_rms, fig_A, fig_A_imp_rms, fig_A_unalt_rms
-
-	"""
 
 
 
