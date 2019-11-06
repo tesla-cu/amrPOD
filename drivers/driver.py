f67685c1d31161624938f93370eb2c7ede4d6a2e
Mike Meehan <michael.a.meehan@colorado.edu>
2019-11-06 15:32:42 -0700

    finalized driver function

--- drivers/driver.py
+++ drivers/driver.py
@@ -9,14 +9,12 @@ sys.path.insert(1, '../plotting/')
 from Compute_POD import Compute_POD
 from plot_single import plot_single
 
-
-
 # This function simply enables us to do a parallel run where we have
 # multiple arguments for the function
 def parallel_run(args):
    return Compute_POD(*args)
 
-
+# ========== Main function ==========================================
 if __name__ == '__main__':
 
     print('starting script to perform POD on AMR grids ...')
@@ -25,27 +23,14 @@ if __name__ == '__main__':
     gen_grid    = True  # are we generating synthetic data?
     compute_tc  = True  # are we computing the time complexity?
     compute_cpu = False # are we computing the cpu time?
-    nx          = 64   # x spatial points                  
-    ny          = 64    # y spatial points
-    nz          = 32     # z spatial points
-    finest      = 5     # finest level of AMR in the domain
+    nx          = 32   # x spatial points                  
+    ny          = 32   # y spatial points
+    nz          = 1     # z spatial points
+    finest      = 1     # finest level of AMR in the domain
     nsample     = 8    # number of samples for each parameter set
-    # nt_arr      = np.arange(101, 102, 5)     # spanning nt
-    nt_arr      = np.arange(5, 11, 5)     # spanning nt
-    l1_arr      = np.arange(0.0, 1*0.0625, 0.0625) # spanning l1
+    nt_arr      = np.arange(2, 62, 20)     # spanning nt
+    l1_arr      = np.arange(0.0, 50/64, 16/64) # spanning l1
     lcs         = np.zeros((finest+1)) # fraction of grid that stays constant in time
-    # lc_fracs     = np.array([1/16, 0/16, 0/16])
-
-    # Variables we will iterate through
-    xvals = nt_arr # what will be plotted on the x-axis
-    yvals = l1_arr # what will be plotted on the y-axis
-
-    
-    # lc_fracs     = np.array([1/16, 0/16, 0/16])
-    # l0_frac_arr  = np.zeros(np.size(rc_arr))
-    # l1_frac_arr  = np.zeros(np.size(rc_arr))
-    # l2_frac_arr  = np.zeros(np.size(rc_arr))
-    # l_frac_data  = np.zeros((np.size(l1_frac_arr), nlev))
 
     # Direction where /code/ livesc
     # basedir = '/Users/samsimonswellin/desktop/'
@@ -54,7 +39,6 @@ if __name__ == '__main__':
     # Directory where AMR data is stored
     # amr_datadir = '/Users/samsimonswellin/desktop/' + \
     #     'x0.000-0.000_y-1.000-1.000_z0.000-2.000_t40.0000-42.0000/'
-
     amr_datadir = '/Users/mikemeehan/Research/InternalResearchPapers/AMR_POD/data/' + \
         'slice/x0.000-0.000_y-1.000-1.000_z0.000-2.000_t40.0000-42.0000/'
 
@@ -64,7 +48,7 @@ if __name__ == '__main__':
         os.mkdir(datadir)
 
     # Directory that describes the study we are looking at
-    studydir = datadir + 'nt_l1_small/'
+    studydir = datadir + 'l1_nt/'
     if not os.path.exists(studydir):
         os.mkdir(studydir)
 
@@ -77,6 +61,12 @@ if __name__ == '__main__':
     imgdir = studydir + 'images/'
     if not os.path.exists(imgdir):
         os.mkdir(imgdir)
+
+    # ---------- Useful things based on inputs ----------------------
+
+    # Variables we will iterate through
+    xvals = nt_arr # what will be plotted on the x-axis
+    yvals = l1_arr # what will be plotted on the y-axis
 
     # Useful things we will need
     ndim  = 0          # num dimensions
@@ -111,31 +101,13 @@ if __name__ == '__main__':
         CPU_rms_imp   = np.zeros((len(xvals), len(yvals), 4))
         CPU_rms_unalt = np.zeros((len(xvals), len(yvals), 4))
 
-    # TC_R_imp_avg       = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_R_unalt_avg     = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_R_imp_rms       = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_R_unalt_rms     = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-
-    # TC_Phi_1_imp_avg   = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_Phi_1_unalt_avg = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_Phi_1_imp_rms   = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_Phi_1_unalt_rms = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-
-    # TC_Phi_5_imp_avg   = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_Phi_5_unalt_avg = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_Phi_5_imp_rms   = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_Phi_5_unalt_rms = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-
-    # TC_A_imp_avg       = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_A_unalt_avg     = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_A_imp_rms       = np.zeros((np.size(rc_arr), np.size(nt_arr)))
-    # TC_A_unalt_rms     = np.zeros((np.size(rc_arr), np.size(nt_arr)))
 
     # Start parallel processing
     nthread = mp.cpu_count()
-    # nthread = 4
     print('starting pool with %i threads ...' % nthread)
     pool = mp.Pool(processes=nthread)
+
+    # ========== Begin computation of various parameters ============
 
     for ixval, xval in enumerate(xvals):
         for iyval, yval in enumerate(yvals):
@@ -144,37 +116,24 @@ if __name__ == '__main__':
 
             print('nt = %i, l1 = %0.4f' % (nt, l1))
 
-            # Since we have l in our vars, we need to ls
+            # Since we have l in our vars, we need to get ls
             ls = np.array([1.0-l1, l1])
-            # ls = np.array([1.0-3*l1, l1, l1, l1/2, l1/2])
-            # ls = np.array([1.0-3*l1, l1, l1, l1])
 
-            # ls = np.array([0.5, 0.25, 0.125, 0.0625, 0.0625])
-            # ls = np.array([0.5, 0.25, 0.125, 0.125])
-            # ls = np.array([0.5, 0.25, 0.25])
-            # ls = np.array([0.5, 0.5])
-
-
-            # ls = np.array([1.0-l1-0.125-0.0625, l1, 0.0625, 0.0625, 0.0625])
-            # l_fracs       = l_frac_data[irc_arr,:] 
-
-            # ========== Compute time complexity
+            # ---------- Compute time complexity --------------------
             if compute_tc:
                 # Generate tuple that stores all relevant parameters
                 #    - note, we need list of these equal to number of samples
                 if gen_grid:
-                    arg_tuple = (gen_grid, nx, ny, nz, finest, ls, lcs, nt)
+                    arg_tuple = (gen_grid, nx, ny, nz, finest, ls, lcs, nt, \
+                        'TC')
                 else:
                     arg_tuple = (gen_grid, nx, ny, nz, finest, ls, lcs, nt, \
                         'TC', amr_datadir)
                 arg_list = []
                 [arg_list.append(arg_tuple) for i in range(nsample)]
-                # print(arg_list)
 
                 # Farm out nsample to each processor
                 res_tuple = pool.map(parallel_run, arg_list)
-                # print('res_tuple = ', res_tuple)
-                # print('comp ratio = ', rc_arr[irc_arr])
 
                 for i in range(8):
                     res = np.zeros((nsample), dtype=int)
@@ -184,8 +143,6 @@ if __name__ == '__main__':
 
                     res_avg = np.mean(res)
                     res_rms = np.std(res-res_avg)
-                    # print('res_avg = ', res_avg)
-                    # print('res_rms =', res_rms)
 
                     if   i==0: 
                         TC_avg_imp[ixval, iyval, 0]   = res_avg
@@ -212,7 +169,7 @@ if __name__ == '__main__':
                         TC_avg_unalt[ixval, iyval, 3] = res_avg
                         TC_rms_unalt[ixval, iyval, 3] = res_rms
 
-            # ========== Compute CPU time
+            # ---------- Compute CPU time ---------------------------
             if compute_cpu:
                 # Generate tuple that stores all relevant parameters
                 #    - note, we need list of these equal to number of samples
@@ -224,12 +181,9 @@ if __name__ == '__main__':
                         'CPU', amr_datadir)
                 arg_list = []
                 [arg_list.append(arg_tuple) for i in range(nsample)]
-                # print(arg_list)
 
                 # Farm out nsample to each processor
                 res_tuple = pool.map(parallel_run, arg_list)
-                # print('res_tuple = ', res_tuple)
-                # print('comp ratio = ', rc_arr[irc_arr])
 
                 for i in range(8):
                     res = np.zeros((nsample))
@@ -239,8 +193,6 @@ if __name__ == '__main__':
 
                     res_avg = np.mean(res)
                     res_rms = np.std(res-res_avg)
-                    # print('res_avg = ', res_avg)
-                    # print('res_rms =', res_rms)
 
                     if   i==0: 
                         CPU_avg_imp[ixval, iyval, 0]   = res_avg
@@ -270,7 +222,7 @@ if __name__ == '__main__':
     pool.close()
     pool.join()
 
-    # ========== Save data
+    # ========== Save data ==========================================
 
     # Write out data from this run
     sim_info = open(txtdir + '/sim_info.txt', 'w')
@@ -280,19 +232,16 @@ if __name__ == '__main__':
     sim_info.write("nx:       %i\n" % nx)
     sim_info.write("ny:       %i\n" % ny)
     sim_info.write("nz:       %i\n" % nz)
-    sim_info.write("xval:     %s\n" % "nt")
+    sim_info.write("xvar:     %s\n" % "nt")
     [sim_info.write("lc%i:      %0.8f\n" % (i,lc)) for i,lc in enumerate(lcs)]
     sim_info.write("x_0:      %i\n" % xvals[0])
     sim_info.write("x_inc:    %i\n" % np.diff(xvals[0:2]))
     sim_info.write("x_end:    %i\n" % xvals[-1])
-    sim_info.write("yval:     %s\n" % "l1")
+    sim_info.write("yvar:     %s\n" % "l1")
     sim_info.write("y_0:      %i\n" % yvals[0])
-    # sim_info.write("y_inc:    %i\n" % np.diff(yvals[0:2]))
+    sim_info.write("y_inc:    %i\n" % np.diff(yvals[0:2]))
     sim_info.write("y_end:    %i\n" % yvals[-1])
     sim_info.close()
-
-    # np.savetxt(txtdir + "/rc_array.txt", rc_arr)
-    # np.savetxt(txtdir + "/nt_array.txt", nt_arr)
 
     # Write out time complexity data
     if compute_tc:
@@ -340,7 +289,7 @@ if __name__ == '__main__':
 
     
     # Plot these various quantities
-    # plot_single(txtdir, imgdir, compute_tc, compute_cpu, xvals, yvals, 'nt', 'l1')
+    plot_single(txtdir, imgdir, compute_tc, compute_cpu, xvals, yvals, 'nt', 'l1')
 
 
     # Code if we want to span rc
