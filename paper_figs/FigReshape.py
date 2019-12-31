40ead75bbab2f99c5313d00e416ef9b219d99963
Mike Meehan <michael.a.meehan@colorado.edu>
2019-12-31 09:49:10 -0700

    refactorization. adjustments to make figure look good in paper.

--- paper_figs/FigReshape.py
+++ paper_figs/FigReshape.py
@@ -3,23 +3,25 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 from matplotlib import gridspec
 import matplotlib.patches as patches
+from matplotlib.ticker import FormatStrFormatter
 
 def FigReshape(imgdir):
 
     # Set up figure
-    fig = plt.figure()
+    fig = plt.figure(figsize=(6.5, 3.25))
     # Define grid size in image. 
     # If this changes, colspan and rowspan will need to change.
-    gridspec.GridSpec(256, 256)     
+    gs = gridspec.GridSpec(132, 264)     
     d = .1 # dot diameter for 3 continuation dots
-    # define colormap
+    ax_label_skip = 2 # how often we tick the axes
+
+    # Define colormap ---------------------------------------------------------
     cmap_acc = mpl.cm.get_cmap('Accent')
     lvlC0    = np.tile(cmap_acc(.125), (3,1))
     lvlC1    = np.tile(cmap_acc(.375), (3,1))
     lvlC2    = np.tile(cmap_acc(.625), (3,1))
     lvlC3    = np.tile(cmap_acc(.875), (4,1))
-
-    # Adjust color map to include proper number of colors
+    # adjust color map to vary solution values 
     for i in range(4):
         if i < 3:
             lvlC0[i,1] = lvlC0[i,1] - (i*.1)
@@ -27,13 +29,14 @@ def FigReshape(imgdir):
             lvlC2[i,0] = lvlC2[i,0] - (i*.1)
         if i < 4:
             lvlC3[i,0] = lvlC3[i,0] - (i*.1)
+    cmap1 = mpl.colors.ListedColormap([lvlC0[0,:], lvlC0[1,:], lvlC0[2,:],\
+                                       lvlC1[0,:], lvlC1[1,:], lvlC1[2,:],\
+                                       lvlC2[0,:], lvlC2[1,:], lvlC2[2,:],\
+                                       lvlC3[0,:], lvlC3[1,:], lvlC3[2,:], lvlC3[3,:]])
 
-    cmap1    = mpl.colors.ListedColormap([lvlC0[0,:], lvlC0[1,:], lvlC0[2,:],\
-                                          lvlC1[0,:], lvlC1[1,:], lvlC1[2,:],\
-                                          lvlC2[0,:], lvlC2[1,:], lvlC2[2,:],\
-                                          lvlC3[0,:], lvlC3[1,:], lvlC3[2,:], lvlC3[3,:]])
+    # Generate synthetic X ----------------------------------------------------
 
-    # Initialize X
+    # Initialize
     X = np.zeros((16, 16))
 
     # Level zero
@@ -57,7 +60,7 @@ def FigReshape(imgdir):
     X[13:14, 11:12] = 11/13
     X[12:13, 11:12] = 12/13
 
-    # Set up of reshaping procedure
+    # Set up of reshaping procedure -------------------------------------------
     nx     = 16
     ny     = 16
     nspat  = int(nx*ny)
@@ -69,22 +72,30 @@ def FigReshape(imgdir):
         c_l[i] = 2**(finest-i)
     data_1D = np.squeeze(X)
 
-    plt.subplot2grid((256,256), (0, 0), colspan=64, rowspan=64) 
-    plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
-    plt.box(False)
 
     # Plot original grid
+    plt.subplot2grid((132,264), (0, 0), colspan=64, rowspan=64)
+    plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
+    plt.box(False)
     ax = plt.gca()
-    ax.set_xticks(np.arange(0, nx, 1))
-    ax.set_yticks(np.arange(0, ny, 1))
+    ax.set_xticks(np.arange(ax_label_skip-1, nx, ax_label_skip))
+    ax.set_yticks(np.arange(ax_label_skip-1, ny, ax_label_skip))
+    ax.set_title('Initial Grid')
+    xticklabels = []
+    [xticklabels.append(('%i' % i)) \
+        for i in np.arange(ax_label_skip, nx+1, ax_label_skip)]
+    ax.set_xticklabels(xticklabels, fontsize='x-small')
+    yticklabels = []
+    [yticklabels.append(('%i' % i)) \
+        for i in np.arange(ax_label_skip, ny+1, ax_label_skip)]
+    ax.set_yticklabels(yticklabels, fontsize='x-small')
+    ax.tick_params(labelbottom=True, labelleft=True, right=False, top=False)
     ax.set_xticks(np.arange(-.5, nx, 1), minor=True);
     ax.set_yticks(np.arange(-.5, ny, 1), minor=True);
     ax.grid(which='minor', color='k', linestyle='--', linewidth=.25)
-    ax.tick_params(labelbottom=False)
-    ax.tick_params(labelleft=False)
-    ax.tick_params(which='minor', axis='y', colors='white')
-    ax.tick_params(which='minor', axis='x', colors='white')
-    ax.arrow(16, 7.5, 6, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
+    ax.tick_params(which='minor', direction='out', length=0.1, colors='white')
+    ax.tick_params(which='major', direction='out', length=3.0)
+    ax.arrow(16, 7.5, 3, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
 
     nxr = nx
 
@@ -100,89 +111,61 @@ def FigReshape(imgdir):
         nxr = data_1D.shape[0]
         nyr = nspat/nxr
         
+        # Plot data
         if k == 0:
-            plt.subplot2grid((256,256), (0, 80), colspan=32, rowspan=128)
-            plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
-            plt.box(False)
+            plt.subplot2grid((132,264), (0, 97), colspan=32, rowspan=128)
+        elif k == 1:
+            plt.subplot2grid((132,264), (0, 162), colspan=16, rowspan=132)
+        elif k == 2:
+            plt.subplot2grid((132,264), (0, 215), colspan=8,  rowspan=132)
+        elif k == 3:
+            plt.subplot2grid((132,264), (0, 260), colspan=4,  rowspan=132)
 
-            ax = plt.gca()
-            ax.set_xticks(np.arange(0, nxr, 1))
-            ax.set_yticks(np.arange(0, nyr, 1))
-            ax.set_xticks(np.arange(-.5, nxr, 1), minor=True);
-            ax.set_yticks(np.arange(-.5, nyr, 1), minor=True);
-            ax.grid(which='minor', color='k', linestyle='--', linewidth=.25)
-            ax.tick_params(labelbottom=False)
-            ax.tick_params(labelleft=False)
-            ax.tick_params(which='minor', axis='y', colors='white')
-            ax.tick_params(which='minor', axis='x', colors='white')
-            ax.arrow(8, 23.5, 6, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
+        plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
+        plt.box(False)
+        ax = plt.gca()
+        ax.set_title('Iteration %i' % (k+1))
+        ax.set_yticks(np.arange(ax_label_skip-1, nyr, ax_label_skip))
+        yticklabels = []
+        [yticklabels.append(('%i' % i)) \
+            for i in np.arange(ax_label_skip, nyr+1, ax_label_skip)]
+        ax.set_yticklabels(yticklabels, fontsize='x-small')
+        ax.tick_params(labelbottom=True, labelleft=True, right=False, top=False)
+        ax.set_xticks(np.arange(-.5, nxr, 1), minor=True);
+        ax.set_yticks(np.arange(-.5, nyr, 1), minor=True);
+        ax.grid(which='minor', color='k', linestyle='--', linewidth=.25)
+        ax.tick_params(which='minor', direction='out', length=0.1, colors='white')
+        ax.tick_params(which='major', direction='out', length=3.0)
 
-        if k == 1:
-            plt.subplot2grid((256,256), (0, 134), colspan=16, rowspan=200)
-            plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
-            plt.box(False)
-
-            ax = plt.gca()
-            ax.set_xticks(np.arange(0, nxr, 1))
-            ax.set_yticks(np.arange(0, nyr, 1))
-            ax.set_xticks(np.arange(-.5, nxr, 1), minor=True);
-            ax.set_yticks(np.arange(-.5, nyr, 1), minor=True);
-            ax.grid(which='minor', color='k', linestyle='--', linewidth=.25)
-            ax.tick_params(labelbottom=False)
-            ax.tick_params(labelleft=False)
-            ax.tick_params(which='minor', axis='y', colors='white')
-            ax.tick_params(which='minor', axis='x', colors='white')
-            ax.tick_params(which='major', axis='x', colors='white')
-            ax.set_ylim((13.5,63.5))
-            ax.arrow(4, 55.5, 6, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
+        if k == 0:
+            ax.set_xticks(np.arange(ax_label_skip-1, nxr, ax_label_skip))
+            xticklabels = []
+            [xticklabels.append(('%i' % i)) \
+                for i in np.arange(ax_label_skip, nxr+1, ax_label_skip)]
+            ax.set_xticklabels(xticklabels, fontsize='x-small')
+            ax.arrow(8, 23.5, 3, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
+        elif k == 1:
+            ax.set_xticks([])
+            ax.set_ylim((30.5,63.5))
+            ax.arrow(4, 55.5, 3, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
             for i in range(3):
-                circ = plt.Circle((1.5, 12.5-i), d, color='k', clip_on=False)
+                circ = plt.Circle((1.5, 29.5-i), d, color='k', clip_on=False)
+                ax.add_patch(circ)
+        elif k == 2:
+            ax.set_xticks([])
+            ax.set_ylim((94.5,127.5))
+            ax.arrow(2, 119.5, 3, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
+            for i in range(3):
+                circ = plt.Circle((.5, 93.5-i), d, color='k', clip_on=False)
+                ax.add_patch(circ)  
+        elif k == 3:
+            ax.set_xticks([])
+            ax.set_ylim((222.5,255.5))
+            ax.set_title('Final Vector')
+            for i in range(3):
+                circ = plt.Circle((0, 221.5-i), d, color='k', clip_on=False)
                 ax.add_patch(circ)
 
-        if k == 2:
-            plt.subplot2grid((256,256), (0, 175), colspan=8, rowspan=220)
-            plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
-            plt.box(False)
-
-            ax = plt.gca()
-            ax.set_xticks(np.arange(0, nxr, 1))
-            ax.set_yticks(np.arange(0, nyr, 1))
-            ax.set_xticks(np.arange(-.5, nxr, 1), minor=True);
-            ax.set_yticks(np.arange(-.5, nyr, 1), minor=True);
-            ax.grid(which='minor', color='k', linestyle='--', linewidth=.15)
-            ax.tick_params(labelbottom=False)
-            ax.tick_params(labelleft=False)
-            ax.tick_params(which='minor', axis='y', colors='white')
-            ax.tick_params(which='minor', axis='x', colors='white') 
-            ax.tick_params(which='major', axis='x', colors='white')
-            ax.set_ylim((72.5,127.5))
-            ax.arrow(2, 119.5, 6, 0, head_width=.5, head_length=1, fc='k', ec='k', clip_on=False)
-            for i in range(3):
-                circ = plt.Circle((.5, 71.5-i), d, color='k', clip_on=False)
-                ax.add_patch(circ)      
-        
-        
-        if k==3:
-
-            plt.subplot2grid((256,256), (0, 210), colspan=4, rowspan=260)
-            plt.imshow(data_1D.T, cmap=cmap1, origin='lower')
-            plt.box(False)
-
-            ax = plt.gca()
-            ax.set_xticks(np.arange(-.5, 1, 1))
-            ax.set_yticks(np.arange(0, 256.5, 1))
-            ax.set_xticks(np.arange(-.5, 1, 1), minor=True);
-            ax.set_yticks(np.arange(-.5, 256.5, 1), minor=True);
-            ax.grid(which='minor', color='k', linestyle='--', linewidth=.15)
-            ax.tick_params(labelbottom=False)
-            ax.tick_params(labelleft=False)
-            ax.tick_params(which='minor', axis='y', colors='white')
-            ax.tick_params(which='minor', axis='x', colors='white') 
-            ax.tick_params(which='major', axis='x', colors='white')
-            ax.set_ylim((190.5,255.5))
-            for i in range(3):
-                circ = plt.Circle((0, 189.5-i), d, color='k', clip_on=False)
-                ax.add_patch(circ)
-
+    # gs.tight_layout(fig, rect=[0,0,1,1])
     fig.set_size_inches(6.5,3.25,forward=True)
     plt.savefig(imgdir + 'fig_reshape.png', dpi=300)
