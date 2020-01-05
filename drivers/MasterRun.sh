ec0289f8a867a460a9097cd06ed41931582cf7e8
Mike Meehan <michael.a.meehan@colorado.edu>
2020-01-05 16:07:31 -0700

    master run to run all driver functions

---
+++ drivers/MasterRun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python driver_l1_nt.py
+python driver_l1_nt_3D.py
+python driver_lc0_lc1.py
+python driver_l1_l0.py
+python driver_lc1l1_lc0l0.py
+python driver_l0_l1_l2_3D.py
+python driver_lc0l0_lc1l1_lc2l2_3D.py
