01305f48902d8d310065038d8f716475b51daa34
Mike Meehan <mime5507@colorado.edu>
2020-06-26 12:41:06 -0600

    minor updates to syntex

--- tests/reshaping/column_order_reshape.m
+++ tests/reshaping/column_order_reshape.m
@@ -6,10 +6,8 @@ finest = 1;
 
 % Repetition information
 c_l    = zeros(finest+1, 1);
-d_l    = zeros(finest+1, 1);
 for i = 0:finest
     c_l(i+1) = 2^(finest - i);
-    d_l(i+1) = (2^dim)^(finest-i);
 end
 
 % =========================================================================
@@ -23,7 +21,7 @@ if dim == 2
     grid(1:2, 3:4) = 1;
     grid(5:6, 3:4) = 2;
     
-    % Reshape into colomn vector
+    % Reshape into column vector
     for i = 1 : numel(c_l)
         nxr  = size(grid, 1);
         nyr  = size(grid, 2);
@@ -49,7 +47,6 @@ if dim == 2
     end
     nxr  = size(grid, 1);
     nyr  = size(grid, 2);
-    c = c(1);
     grid = reshape(grid, ny, [], nyr);
     grid = permute(grid, [1 3 2]);
     grid = reshape(grid, ny, nx);
@@ -68,13 +65,12 @@ else
     grid(3:4, 1:2, 1:2) = 1;
     nspat = numel(grid);
     
-    % Reshape into colomn vector
+    % Reshape into column vector
     for i = 1 : numel(c_l)
-        c = c_l(i);
-        nsize   = size(grid);
-        nxr     = nsize(1);
-        nyr     = nsize(2);
-        nzr     = nsize(3);
+        c       = c_l(i);
+        nxr     = size(grid, 1);
+        nyr     = size(grid, 2);
+        nzr     = size(grid, 3);
         nrshp_1 = nspat / (nzr*nyr*c);
         nrshp_2 = nspat / (nzr*c^2);
         nrshp_3 = nspat / c^2;
@@ -92,24 +88,31 @@ else
     
     % Reshape into original grid
     for i = numel(c_l)-1 : -1 : 1 
-        nxr  = size(grid, 1);
-        nyr  = size(grid, 2);
-        nzr  = size(grid, 3);
-        c    = c_l(i);
-        grid = reshape(grid, c, [], nyr, nzr);
+        c       = c_l(i);
+        nxr     = size(grid, 1);
+        nyr     = size(grid, 2);
+        nzr     = size(grid, 3);
+        nrshp_1 = nspat / (c*nyr*nzr);
+        nrshp_2 = nspat / (c*c*nzr);
+        nrshp_3 = nspat / (c*c);
+        
+        grid = reshape(grid, c, nrshp_1, nyr, nzr);
         grid = permute(grid, [1 3 2 4]);
-        grid = reshape(grid, c, c, [], nzr);
+        grid = reshape(grid, c, c, nrshp_2, nzr);
         grid = permute(grid, [1 2 4 3]);
-        grid = reshape(grid, c, c, []);
+        grid = reshape(grid, c, c, nrshp_3);
         grid = permute(grid, [3 2 1]);
     end
-    nxr  = size(grid, 1);
-    nyr  = size(grid, 2);
-    nzr  = size(grid, 3);
-    c = c(1);
-    grid = reshape(grid, nz, [], nyr, nzr);
+    
+    nxr     = size(grid, 1);
+    nyr     = size(grid, 2);
+    nzr     = size(grid, 3);
+    nrshp_1 = nspat/(nz*nyr*nzr);
+    nrshp_2 = nspat/(nz*ny*nzr);
+    
+    grid = reshape(grid, nz, nrshp_1, nyr, nzr);
     grid = permute(grid, [1 3 2 4]);
-    grid = reshape(grid, nz, ny, [], nzr);
+    grid = reshape(grid, nz, ny, nrshp_2, nzr);
     grid = permute(grid, [1 2 4 3]);
     grid = reshape(grid, nz, ny, nx);
     grid = permute(grid, [3 2 1]);
