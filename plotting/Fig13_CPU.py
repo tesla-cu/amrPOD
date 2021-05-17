5b877b28bbf07f1beb5647fafd29bf8ceac7e349
mameehan5 <mime5507@colorado.edu>
2021-05-17 17:01:25 -0600

    add f=3

--- plotting/Fig13_CPU.py
+++ plotting/Fig13_CPU.py
@@ -16,19 +16,19 @@ def Fig13_CPU(datadir, imgdir):
     fig = plt.figure()
 
     # Set up data directories
-    f1dir = datadir + 'AMR_gen/f1_3D/'
-    f2dir = datadir + 'AMR_gen/f2_3D/'
+    f1dir = datadir + 'AMR_gen/f1_3D/intel_timing/'
+    f2dir = datadir + 'AMR_gen/f2_3D/intel_timing/'
+    f3dir = datadir + 'AMR_gen/f3_3D/intel_timing/'
 
     # Data information
     ntmin = 10
     ntmax = 80
     ntinc = 10
-
     nt = np.arange(ntmin, ntmax+ntinc, ntinc)
 
     # Image set up
-    grid = AxesGrid(fig, (0.10,0.10,0.84,0.82),
-        nrows_ncols = (2, 3),
+    grid = AxesGrid(fig, (0.08,0.075,0.91,0.89),
+        nrows_ncols = (3, 3),
         axes_pad    = 0.2,
         aspect      = False,
         label_mode  = "L",
@@ -40,20 +40,22 @@ def Fig13_CPU(datadir, imgdir):
     #  - index 1: nt
     #  - index 2: num of finest levels
     #  - index 3: first is avg, second is rms
-    R_AMR    = np.zeros((len(nt), 2, 2))
-    R_std    = np.zeros((len(nt), 2, 2))
-    Phi_AMR1 = np.zeros((len(nt), 2, 2))
-    Phi_AMR2 = np.zeros((len(nt), 2, 2))
-    Phi_std  = np.zeros((len(nt), 2, 2))
-    A_AMR    = np.zeros((len(nt), 2, 2))
-    A_std    = np.zeros((len(nt), 2, 2))
+    R_AMR    = np.zeros((len(nt), 3, 2))
+    R_std    = np.zeros((len(nt), 3, 2))
+    Phi_AMR1 = np.zeros((len(nt), 3, 2))
+    Phi_AMR2 = np.zeros((len(nt), 3, 2))
+    Phi_std  = np.zeros((len(nt), 3, 2))
+    A_AMR    = np.zeros((len(nt), 3, 2))
+    A_std    = np.zeros((len(nt), 3, 2))
 
     for i, n in enumerate(nt):
-        for j in range(2):
+        for j in range(3):
             if j==0:
                 fold = (f1dir + 'CPU_timing_nt%i/' % n)
             elif j==1:
                 fold = (f2dir + 'CPU_timing_nt%i/' % n)
+            elif j==2:
+                fold = (f3dir + 'CPU_timing_nt%i/' % n)
 
             # CPU times for R
             CPUs = np.loadtxt(fold + 'R_CPU_AMR.txt')
@@ -78,11 +80,6 @@ def Fig13_CPU(datadir, imgdir):
 
     # Plot R CPU times for f1 -------------------------------------------------
     ax = grid[0].axes
-
-    # ax.errorbar(nt, R_std[:,0,0], yerr=2*R_std[:,0,1], linestyle='-', \
-    #     elinewidth=1, capsize=5, color='k', label='$\overline{T}_s$')
-    # ax.errorbar(nt, R_AMR[:,0,0], yerr=2*R_AMR[:,0,1], linestyle='--', \
-    #     elinewidth=1, capsize=5, color='b', label='$\overline{T}_a$')
     ax.plot(nt, R_std[:,0,0], linestyle='-', \
         color='k', label='$\overline{T}_s$')
     ax.plot(nt, R_AMR[:,0,0], linestyle='--', \
@@ -98,16 +95,10 @@ def Fig13_CPU(datadir, imgdir):
     ax.set_xlabel(r'$n_t$')
     ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
     ax.set_xlim(10,80)
-    ax.legend(loc=4, frameon=True, facecolor='white', framealpha=1.0, \
-        bbox_to_anchor=(1.1,-0.1))
+    ax.legend(loc=2, frameon=True, facecolor='white', framealpha=1.0)
 
     # Plot R CPU times for f2 -------------------------------------------------
     ax = grid[1].axes
-
-    # ax.errorbar(nt, R_std[:,1,0], yerr=2*R_std[:,1,1], linestyle='-', \
-    #     elinewidth=1, capsize=5, color='k', label='$\overline{T}_s$')
-    # ax.errorbar(nt, R_AMR[:,1,0], yerr=2*R_AMR[:,1,1], linestyle='--', \
-    #     elinewidth=1, capsize=5, color='b', label='$\overline{T}_a$')
     ax.plot(nt, R_std[:,1,0], linestyle='-', \
         color='k', label='$\overline{T}_s$')
     ax.plot(nt, R_AMR[:,1,0], linestyle='--', \
@@ -124,15 +115,26 @@ def Fig13_CPU(datadir, imgdir):
     ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
     ax.set_xlim(10,80)
 
-    # Plot Phi CPU times for f1 -----------------------------------------------
+    # Plot R CPU times for f3 -------------------------------------------------
     ax = grid[2].axes
+    ax.plot(nt, R_std[:,2,0], linestyle='-', \
+        color='k', label='$\overline{T}_s$')
+    ax.plot(nt, R_AMR[:,2,0], linestyle='--', \
+        color='b', label='$\overline{T}_a$')
+    ax.set_title(r'(g) $\bf{R}$, $f=3$', loc='left')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.grid('on', linestyle=':')
+    
+    # Label information
+    ax.set_xticks(nt)
+    ax.set_xticklabels(['10','20','','40','','60','','80'])
+    ax.set_xlabel(r'$n_t$')
+    ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
+    ax.set_xlim(10,80)
 
-    # ax.errorbar(nt, Phi_std[:,0,0], yerr=2*Phi_std[:,0,1], linestyle='-', \
-    #     elinewidth=1, capsize=5, color='k', label='$\overline{T}_s$')
-    # ax.errorbar(nt, Phi_AMR1[:,0,0], yerr=2*Phi_AMR1[:,0,1], linestyle='--', \
-    #     elinewidth=1, capsize=5, color='b', label='$\overline{T}_a$ -- M1')
-    # ax.errorbar(nt, Phi_AMR2[:,0,0], yerr=2*Phi_AMR2[:,0,1], linestyle=':', \
-    #     elinewidth=1, capsize=5, color='orange',label='$\overline{T}_a$ -- M2')
+    # Plot Phi CPU times for f1 -----------------------------------------------
+    ax = grid[3].axes
     ax.plot(nt, Phi_std[:,0,0], linestyle='-', \
         color='k', label='$\overline{T}_s$')
     ax.plot(nt, Phi_AMR1[:,0,0], linestyle='--', \
@@ -150,18 +152,10 @@ def Fig13_CPU(datadir, imgdir):
     ax.set_xlabel(r'$n_t$')
     ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
     ax.set_xlim(10,80)
-    ax.legend(loc=4, frameon=True, facecolor='white', framealpha=1.0, \
-        bbox_to_anchor=(1.1,-0.1))
+    ax.legend(loc=2, frameon=True, facecolor='white', framealpha=1.0)
 
     # Plot Phi CPU times for f2 -----------------------------------------------
-    ax = grid[3].axes
-
-    # ax.errorbar(nt, Phi_std[:,1,0], yerr=2*Phi_std[:,1,1], linestyle='-', \
-    #     elinewidth=1, capsize=5, color='k', label='$\overline{T}_s$')
-    # ax.errorbar(nt, Phi_AMR1[:,1,0], yerr=2*Phi_AMR1[:,1,1], linestyle='--', \
-    #     elinewidth=1, capsize=5, color='b', label='$\overline{T}_a$ -- M1')
-    # ax.errorbar(nt, Phi_AMR2[:,1,0], yerr=2*Phi_AMR2[:,1,1], linestyle=':', \
-    #     elinewidth=1, capsize=5, color='orange',label='$\overline{T}_a$ -- M2')
+    ax = grid[4].axes
     ax.plot(nt, Phi_std[:,1,0], linestyle='-', \
         color='k', label='$\overline{T}_s$')
     ax.plot(nt, Phi_AMR1[:,1,0], linestyle='--', \
@@ -180,9 +174,28 @@ def Fig13_CPU(datadir, imgdir):
     ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
     ax.set_xlim(10,80)
 
-    # Plot A CPU times for f1 -------------------------------------------------
-    ax = grid[4].axes
+    # Plot Phi CPU times for f3 -----------------------------------------------
+    ax = grid[5].axes
+    ax.plot(nt, Phi_std[:,2,0], linestyle='-', \
+        color='k', label='$\overline{T}_s$')
+    ax.plot(nt, Phi_AMR1[:,2,0], linestyle='--', \
+        color='b', label='$\overline{T}_a$ -- M1')
+    ax.plot(nt, Phi_AMR2[:,2,0], linestyle=':', \
+        color='orange',label='$\overline{T}_a$ -- M2')
+    ax.set_title(r'(h) $\bf{\Phi}$, $f=3$', loc='left')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.grid('on', linestyle=':')
+    
+    # Label information
+    ax.set_xticks(nt)
+    ax.set_xticklabels(['10','20','','40','','60','','80'])
+    ax.set_xlabel(r'$n_t$')
+    ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
+    ax.set_xlim(10,80)
 
+    # Plot A CPU times for f1 -------------------------------------------------
+    ax = grid[6].axes
     ax.plot(nt, A_std[:,0,0], linestyle='-', \
         color='k', label='$\overline{T}_s$')
     ax.plot(nt, A_AMR[:,0,0], linestyle='--', \
@@ -198,12 +211,10 @@ def Fig13_CPU(datadir, imgdir):
     ax.set_xlabel(r'$n_t$')
     ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
     ax.set_xlim(10,80)
-    ax.legend(loc=4, frameon=True, facecolor='white', framealpha=1.0, \
-        bbox_to_anchor=(1.1,-0.1))
+    ax.legend(loc=2, frameon=True, facecolor='white', framealpha=1.0)
 
     # Plot A CPU times for f2 -------------------------------------------------
-    ax = grid[5].axes
-
+    ax = grid[7].axes
     ax.plot(nt, A_std[:,1,0], linestyle='-', \
         color='k', label='$\overline{T}_s$')
     ax.plot(nt, A_AMR[:,1,0], linestyle='--', \
@@ -220,8 +231,26 @@ def Fig13_CPU(datadir, imgdir):
     ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
     ax.set_xlim(10,80)
 
+    # Plot A CPU times for f3 -------------------------------------------------
+    ax = grid[8].axes
+    ax.plot(nt, A_std[:,2,0], linestyle='-', \
+        color='k', label='$\overline{T}_s$')
+    ax.plot(nt, A_AMR[:,2,0], linestyle='--', \
+        color='b', label='$\overline{T}_a$')
+    ax.set_title(r'(i) $\bf{A}$, $f=3$', loc='left')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.grid('on', linestyle=':')
+    
+    # Label information
+    ax.set_xticks(nt)
+    ax.set_xticklabels(['10','20','','40','','60','','80'])
+    ax.set_xlabel(r'$n_t$')
+    ax.set_ylabel(r'$\overline{T}$ [CPU (s)]')
+    ax.set_xlim(10,80)
+
     # Save figure -------------------------------------------------------------
-    fig.set_size_inches(6.5,3.75,forward=True)
+    fig.set_size_inches(6.5,5.5,forward=True)
     plt.savefig(imgdir + 'Fig13_CPU.png', dpi=600)    
 
     print('\tdone with figure 13')
