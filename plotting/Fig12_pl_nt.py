c81f9b06a9a7bc35a0bcc8dc46622229a2998bc3
mameehan5 <mime5507@colorado.edu>
2021-05-17 16:52:21 -0600

    add f=3

--- plotting/Fig12_pl_nt.py
+++ plotting/Fig12_pl_nt.py
@@ -13,38 +13,36 @@ def Fig12_pl_nt(datadir, imgdir):
     # lvlC0    = np.tile(cmap_acc(.125), (3,1))
     # lvlC1    = np.tile(cmap_acc(.375), (3,1))
     # lvlC2    = np.tile(cmap_acc(.625), (3,1))
-    clrs = [cmap_acc(.125), cmap_acc(.375), cmap_acc(.625)]
-    lins = ['-', ':']
-    mrks = ['o', 's', '^']
+    clrs = [cmap_acc(.125), cmap_acc(.375), cmap_acc(.625), cmap_acc(0.875)]
+    lins = ['-', '--']
+    mrks = ['o', 's', '^','d']
 
     # Set up data directories
-    f1dir = datadir + 'AMR_gen/f1_3D/'
-    f2dir = datadir + 'AMR_gen/f2_3D/'
+    f1dir = datadir + 'AMR_gen/f1_3D/intel_timing/'
+    f2dir = datadir + 'AMR_gen/f2_3D/intel_timing/'
+    f3dir = datadir + 'AMR_gen/f3_3D/intel_timing/'
 
     # Data information
     ntmin = 10
-    ntmax = 60
+    ntmax = 80
     ntinc = 10
 
     nt = np.arange(ntmin, ntmax+ntinc, ntinc)
 
     # Image set up
-    grid = AxesGrid(fig, (0.10,0.17,0.88,0.72),
-        nrows_ncols = (1, 2),
+    grid = AxesGrid(fig, (0.07,0.14,0.91,0.79),
+        nrows_ncols = (1, 3),
         axes_pad    = 0.1,
         aspect      = False,
         label_mode  = "L",
         share_all   = True,
         cbar_mode   = "none")
 
-    for i in range(2):
+    for i in range(3):
 
-        if i==0:
-            ps = np.zeros((2, len(nt)))
-            pms = np.zeros((2, len(nt)))
-        elif i==1:
-            ps = np.zeros((3, len(nt)))
-            pms = np.zeros((3, len(nt)))
+        # Initialize p and pm for each level
+        ps = np.zeros((i+2, len(nt)))
+        pms = np.zeros((i+2, len(nt)))
 
         # Plot grid composition -----------------------------------------------
         ax = grid[i].axes
@@ -55,6 +53,8 @@ def Fig12_pl_nt(datadir, imgdir):
                 fold = (f1dir + 'CPU_timing_nt%i/' % n)
             elif i==1:
                 fold = (f2dir + 'CPU_timing_nt%i/' % n)
+            elif i==2:
+                fold = (f3dir + 'CPU_timing_nt%i/' % n)
 
             ps[:,j]  = np.loadtxt(fold + 'p.txt')
             pms[:,j] = np.loadtxt(fold + 'pm.txt')
@@ -72,25 +72,27 @@ def Fig12_pl_nt(datadir, imgdir):
             ax.grid('on', linestyle=':')
 
             # Label information
-            ax.set_xticks(nt)
-            ax.set_xticklabels(['10','20','30','40','50','60'])
-            ax.set_xlabel(r'$n_t$')
+            ax.set_xticks(np.linspace(ntmin, ntmax, 5))
+            ax.set_xticklabels(['10','','45','','80'])
+            ax.set_xlabel(r'$N_\mathrm{t}$')
             ax.set_ylabel('Grid Composition')
 
         if i==0:
             ax.set_title(r'(a) $f=1$', loc='left')
         elif i==1:
             ax.set_title(r'(b) $f=2$', loc='left')
+        elif i==2:
+            ax.set_title(r'(c) $f=3$', loc='left')
     
     # Set legend with ordered elements
     handles, labels = ax.get_legend_handles_labels()
-    order = [0,2,4,1,3,5]
+    order = [0,2,4,6,1,3,5,7]
     ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order], \
-        loc=3, frameon=True, facecolor='white', framealpha=1.0, \
-        ncol=2, bbox_to_anchor=(-0.8,0.05))
+        loc=3, frameon=False, facecolor='None', framealpha=1.0, \
+        ncol=2, bbox_to_anchor=(-2.04,0.09))
 
     # Save figure -------------------------------------------------------------
-    fig.set_size_inches(4.5, 2.0,forward=True)
+    fig.set_size_inches(6.5, 2.6,forward=True)
     plt.savefig(imgdir + 'Fig12_pl_nt.png', dpi=600)    
 
     print('\tdone with figure 12')
